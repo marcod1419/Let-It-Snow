150354b8c3a3ddc35cdf09c89ec3bf62278637c1
Marco <marcod1419@gmail.com>
2017-12-22 16:30:30 -0500

    Responsive fixes

--- js/3d.js
+++ js/3d.js
@@ -14,17 +14,6 @@ class Snowglobe {
     const windowWidth = 800;
     const windowHeight = 450;
 
-    window.addEventListener("resize", function() {
-    var card = document.getElementById("snowman-card");
-
-      if(window.innerWidth > 1095){
-         	 TweenMax.set(card, {x: 300});
-         }
-        else{
-         	 TweenMax.set(card, {x: 0});
-        }
-    });
-
     const container = document.querySelector("#container");
 
     var renderer = new THREE.WebGLRenderer({antialias: true});
@@ -44,15 +33,12 @@ class Snowglobe {
     window.onload = () => {
       var loading = document.getElementById("loading-screen");
 
-      //Disable shadows by default on mobile
-      if(this.mobileCheck()){
-      	document.getElementById("shadows").checked = false;
-      	 for (var i = 0; i < this.lights.length; i++) {
-            if (this.lights[i].type !== "AmbientLight") {
-              renderer.shadowMap.autoUpdate = false;
-              renderer.clearTarget(this.lights[i].shadow.map);
-            }
-          }
+      //Disable shadows by default	
+  	 for (var i = 0; i < this.lights.length; i++) {
+        if (this.lights[i].type !== "AmbientLight") {
+          renderer.shadowMap.autoUpdate = false;
+          renderer.clearTarget(this.lights[i].shadow.map);
+        }
       }
 
       TweenMax.to(loading, 0.5, {autoAlpha: 0});
@@ -119,17 +105,16 @@ class Snowglobe {
     };
     //Add Controls
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    var rotateReset;
     controls.rotateSpeed = 1.0;
     controls.zoomSpeed = 1.5;
-    controls.enablePan = false; //TODO set to false
+    controls.enablePan = false;
     controls.target.set(0, 225, 0);
     controls.enableDamping = true;
     controls.maxPolarAngle = this.degToRad(90);
     controls.autoRotate = true;
     controls.autoRotateSpeed = 0.05;
     controls.minDistance = 250;
-    controls.maxDistance = 8000; //10000
+    controls.maxDistance = 8000;
     controls.addEventListener("change", () => {
       render();
     });
@@ -139,35 +124,35 @@ class Snowglobe {
     container.appendChild(renderer.domElement);
 
     //Table
-    this.addToScene(this.createBox(20000, 800, 10000, 0xffffff, "img/desk.jpg", "img/desk_normal.png", 100, -5000, -695, -300));
+    this.addToScene(this.createBox(20000, 800, 10000, 0xffffff, "img/desk.jpg", "img/desk_normal.png", 100, [-5000, -695, -300]));
 
     //Base
     this.addToScene(this.createGlobeBase(900, 200, 0x845100, "img/globe_base.png", "img/globe_base_normal.png", 0, -201, 0, 0xffffff, "img/ground_snow.jpg", "img/ground_snow_normal.png"));
 
     //Glass
-    this.addToScene(this.createSphere(900, 900, 50, 0xffffff, 0, -102, 0, null, null, "img/glass_alpha.png", scene.background, 0.95, false, false, true));
+    this.addToScene(this.createSphere(900, 900, 50, 0xffffff, [0, -102, 0], null, null, "img/glass_alpha.png", scene.background, 0.95, false, false, true));
 
     //~Body~
-    this.addToScene(this.createSphere(120, 50, 16, 0xffffff, 0, -40, 0, "img/ground_snow.jpg", "img/ground_snow_normal.png"));
-    this.addToScene(this.createSphere(80, 50, 16, 0xffffff, 0, 110, 0, "img/ground_snow.jpg", "img/ground_snow_normal.png"));
-    this.addToScene(this.createSphere(60, 50, 16, 0xffffff, 0, 220, 0, "img/ground_snow.jpg", "img/ground_snow_normal.png"));
+    this.addToScene(this.createSphere(120, 50, 16, 0xffffff, [0, -40, 0], "img/ground_snow.jpg", "img/ground_snow_normal.png"));
+    this.addToScene(this.createSphere(80, 50, 16, 0xffffff, [0, 110, 0], "img/ground_snow.jpg", "img/ground_snow_normal.png"));
+    this.addToScene(this.createSphere(60, 50, 16, 0xffffff, [0, 220, 0], "img/ground_snow.jpg", "img/ground_snow_normal.png"));
 
     //~Face~
 
     //Eyes
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, -16, 240, 54, "img/rock.png", "img/rock_normal.png"));
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, 16, 240, 54, "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [-16, 240, 54], "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [16, 240, 54], "img/rock.png", "img/rock_normal.png"));
 
     //Nose
     this.addToScene(this.createCone(5, 25, 32, 32, 0xffffff, 0, 224, 68, 90, 0, 0, "img/carrot.png", "img/carrot_normal.png", true, false));
 
     //Mouth
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, -20, 214, 58, "img/rock.png", "img/rock_normal.png"));
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, -12, 207, 58, "img/rock.png", "img/rock_normal.png"));
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, -4, 203, 58, "img/rock.png", "img/rock_normal.png"));
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, 4, 203, 58, "img/rock.png", "img/rock_normal.png"));
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, 12, 207, 58, "img/rock.png", "img/rock_normal.png"));
-    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, 20, 214, 58, "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [-20, 214, 58], "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [-12, 207, 58], "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [-4, 203, 58], "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [4, 203, 58], "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [12, 207, 58], "img/rock.png", "img/rock_normal.png"));
+    this.addToScene(this.createSphere(4, 50, 8, 0xffffff, [20, 214, 58], "img/rock.png", "img/rock_normal.png"));
 
     //TopHat
     this.importObj("models/TopHat/", "TopHat.obj", "TopHat.mtl", [20,20,20], [0, 260, 0], [0,0,0], true, false, "Texture_TopHat.PNG");
@@ -226,17 +211,17 @@ class Snowglobe {
     });
 
     //Lights
-    this.lights.push(this.createSpotLight(0xff0000, 0.3, 1000, 50, 1, 0.5, 20, -126, -54, 293, 223, -5, 256, 256));
-    this.lights.push(this.createSpotLight(0x00ff00, 0.3, 1000, 50, 1, 0.5, -20, -126, -54, -293, 223, -5, 256, 256));
-    this.lights.push(this.createSpotLight(0x0000ff, 0.5, 1000, 50, 1, 0.5, 0, 0, -340, 265, 314, -521, 256, 256));
+    this.lights.push(this.createSpotLight(0xff0000, 0.3, 1000, 50, 1, 0.5, [20, -126, -54], [293, 223, -5], 256, 256));
+    this.lights.push(this.createSpotLight(0x00ff00, 0.3, 1000, 50, 1, 0.5, [-20, -126, -54], [-293, 223, -5], 256, 256));
+    this.lights.push(this.createSpotLight(0x0000ff, 0.5, 1000, 50, 1, 0.5, [0, 0, -340], [265, 314, -521], 256, 256));
     // this.lights.push(this.createPointLight(0xfff023, 1, 200, 800, -350));
     // scene.add(new THREE.SpotLightHelper(this.lights[1]));
 
-    this.lights.push(this.createSpotLight(0xffc53f, 0.8, 1000000, 50, 1, 0, 0, 0, 0, 6300, 11719, 9551, 8192, 8192, "RoomLight"));
+    this.lights.push(this.createSpotLight(0xffc53f, 0.8, 1000000, 50, 1, 0, [0, 0, 0], [6300, 11719, 9551], 8192, 8192, "RoomLight"));
 
     this.lights.push(this.setAmbientLight(0xb5f1ff, 0.4));
 
-    var cameraLight = this.createSpotLight(0xffffff, 0.8, 1000000, 1, 1, 0, 0, 0, -500, 1000, 5000, 20000, 8192, 8192);
+    var cameraLight = this.createSpotLight(0xffffff, 0.8, 1000000, 1, 1, 0, [0, 0, -500], [1000, 5000, 20000], 8192, 8192);
     cameraLight.visible = false;
     camera.add(cameraLight);
 
@@ -319,7 +304,7 @@ class Snowglobe {
     };
   }
 
-  createSphere(rad, seg, ring, colour, xPos, yPos, zPos, texturePath, normalMap, alphaMap, envMap, refractionRatio, depthWrite = true, wireframeEnabled = false, isGlobeGlass = false) {
+  createSphere(rad, seg, ring, colour, pos, texturePath, normalMap, alphaMap, envMap, refractionRatio, depthWrite = true, wireframeEnabled = false, isGlobeGlass = false) {
     const RADIUS = rad;
     const SEGMENTS = seg;
     const RINGS = ring;
@@ -366,17 +351,7 @@ class Snowglobe {
       sphere.receiveShadow = false;
     }
 
-    if (xPos) {
-      sphere.position.x = xPos;
-    }
-
-    if (yPos) {
-      sphere.position.y = yPos;
-    }
-
-    if (zPos) {
-      sphere.position.z = zPos;
-    }
+    sphere.position.set(pos[0],pos[1],pos[2])
 
     return sphere;
   }
@@ -431,7 +406,7 @@ class Snowglobe {
     return cone;
   }
 
-  createBox(width, height, depth, colour, texturePath, normalMap, shininess, xPos, yPos, zPos) {
+  createBox(width, height, depth, colour, texturePath, normalMap, shininess, pos) {
     var geometry = new THREE.CubeGeometry(width, height, depth);
 
     var boxMaterial = new THREE.MeshPhongMaterial({});
@@ -451,17 +426,9 @@ class Snowglobe {
     }
 
     var box = new THREE.Mesh(geometry, boxMaterial);
-    if (xPos) {
-      box.position.x = xPos;
-    }
 
-    if (yPos) {
-      box.position.y = yPos;
-    }
+    box.position.set(pos[0], pos[1], pos[2]);
 
-    if (zPos) {
-      box.position.z = zPos;
-    }
     box.castShadow = false;
     box.receiveShadow = true;
 
@@ -671,7 +638,7 @@ class Snowglobe {
     return pointLight;
   }
 
-  createSpotLight(colour, str, distance, angle, blur, decay, xPos, yPos, zPos, xRot, yRot, zRot, shadowW, shadowH, name = "") {
+  createSpotLight(colour, str, distance, angle, blur, decay, pos, rot, shadowW, shadowH, name = "") {
     var spotLight = new THREE.SpotLight(colour, str, distance, this.degToRad(angle), blur, decay);
     spotLight.castShadow = true;
     spotLight.shadow.mapSize.width = shadowW;
@@ -679,12 +646,10 @@ class Snowglobe {
     spotLight.shadow.camera.near = 0.5;
     spotLight.shadow.camera.far = 5000;
 
-    if (xPos && yPos && zPos) {
-      spotLight.target.position.set(xPos, yPos, zPos);
-    }
-    if (xRot && yRot && zRot) {
-      spotLight.position.set(xRot, yRot, zRot);
-    }
+    spotLight.target.position.set(pos[0], pos[1], pos[2]);
+
+    spotLight.position.set(rot[0], rot[1], rot[2]);
+
 
     spotLight.name = name;
 
@@ -773,19 +738,31 @@ class Snowglobe {
         opacity: 1,
         delay: 1,
         onComplete: () => {
-          TweenMax.set(envelope, {autoAlpha: 0});
+          TweenMax.set(envelope, {autoAlpha: 0, display: "none"});
+          TweenMax.set(instructionsCard, {display: "block"});
           if(window.innerWidth > 1095){
          	 TweenMax.set(card, {x: 300});
           }
           else{
-          	TweenMax.set(instructionsCard, {display: "block"});
-            TweenMax.set(mouseInstructions, {display: "none"});
+          	if(this.mobileCheck()){
+            	TweenMax.set(mouseInstructions, {display: "none"});
+          	}
           }
           TweenMax.set(cardContainer, {overflow: "visible"});
           TweenMax.set(instructionsCard, {autoAlpha: 1});
 
           document.getElementById("bgm").volume = 0.7;
     	  document.getElementById("bgm").play();
+
+    	  window.addEventListener("resize", function() {
+		      if(window.innerWidth > 1095){
+		         	 TweenMax.set(card, {x: 300});
+		         }
+		        else{
+		         	 TweenMax.set(card, {x: 0});
+		        }
+		    });
+
         }
       })
     );
@@ -851,7 +828,7 @@ class Snowglobe {
     });
   }
 
-  mobileCheck(){
+   mobileCheck(){
       var check = false;
       (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
       return check;
